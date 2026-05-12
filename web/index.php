5a1f222938780617f57368ce24651aeca25e8fcf
Naiara309 <naiarasancua@gmail.com>
2026-05-12 10:00:05 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,8 +7,8 @@
 <body>
     <div style="text-align: center; margin-top: 50px; font-family: Arial, sans-serif;">
         <h1>Hola, soy Naiara</h1>
-        <p>Esta es la <strong>Versión 1</strong> de la página principal.</p>
-        <p>El despliegue automatizado con contenedores funciona perfectamente.</p>
+        <p>Esta es la <strong>Versión 2</strong> de la página principal.</p>
+        <p>La actualización funciona.</p>
     </div>
 </body>
 </html>
